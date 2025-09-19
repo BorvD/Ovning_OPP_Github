a9e379e73e0a1851598c70da88cca8601242a9aa
BorvD <xenomorph_85@hotmail.com>
2025-09-19 10:20:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ovning_OPP_Github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

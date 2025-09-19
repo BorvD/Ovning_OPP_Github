223d6eaf132e87cbe4961b3532e1bf7cc783dd36
BorvD <xenomorph_85@hotmail.com>
2025-09-19 11:01:36 +0200

    La till för att köra Saving Account. La till mer detaljerade förklaringar

--- Program.cs
+++ Program.cs
@@ -4,15 +4,25 @@
     {
         static void Main(string[] args)
         {
-            // För att köra BankAccount-klassen
-            // Nytt konto med en balans på 100
+            // För att köra BankAccount klassen
+            // Nytt konto med en balans på 100. Går att ändra och testa olika värden inom parenteserna
             BankAccount newAccount = new BankAccount(100);
-            // Sätter in 50 på kontot
+            // Sätter in 50 på kontot. Går att ändra och testa olika värden inom parenteserna
             newAccount.Deposit(50);
-            // Tar ut 25 från kontot
+            // Tar ut 25 från kontot. Går att ändra och testa olika värden inom parenteserna
             newAccount.Withdraw(25);
-            // Visar den aktuella balansen
+            // Visar den aktuella balansen på kontot
             newAccount.ShowBalance();
+
+            // För att köra SavingsAccount klassen
+            // Nytt sparkonto med en balans på 200. Går att ändra och testa olika värden inom parenteserna
+            SavingsAccount newSavingsAccount = new SavingsAccount(200);
+            // Sätter in 100 på sparkontot. Går att ändra och testa olika värden inom parenteserna
+            newSavingsAccount.Deposit(100);
+            // Tar ut 50 från sparkontot. Går att ändra och testa olika värden inom parenteserna
+            newSavingsAccount.Withdraw(50);
+            // Visar den aktuella balansen på sparkontot
+            newSavingsAccount.ShowBalance();
         }
     }
 }

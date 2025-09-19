e591c5c64594f583f0ca09e768e6bf3ca862e567
BorvD <xenomorph_85@hotmail.com>
2025-09-19 11:08:35 +0200

    Glömde helt  att lägga till dokumentation i koden. La till överdrivet mycket dokumentation (hjälper att vara överdriven i det, förklarar då bra exakt för mig vad allt göt i koden)

--- BankAccount.cs
+++ BankAccount.cs
@@ -14,30 +14,35 @@ namespace Ovning_OPP_Github
         // Konstruktor för att initiera kontot med ett startbelopp
         public BankAccount(int Balance)
         {
+            // Sätt kontobalansen till det angivna startbeloppet
             balance = Balance;
         }
 
         // Metod för att sätta in pengar på kontot
         public void Deposit(int amount)
         {
+            // Addera beloppet inom parantereserna till kontobalansen
             balance += amount;
         }
 
         // Metod för att ta ut pengar från kontot
         public void Withdraw(int amount)
         {
+            // Subtrahera beloppet inom parantereserna från kontobalansen
             balance -= amount;
         }
 
         // Metod för att få fram kontobalansen
         public int GetBalance()
         {
+            // Returnera kontobalansen
             return balance;
         }
 
         // Metod för att visa kontobalansen
         public void ShowBalance()
         {
+            // Skriver ut kontobalansen i konsolen
             Console.WriteLine($"Deposit:{Deposit}, Withdraw:{Withdraw}Current balance: {balance}");
         }
     }
--- SavingsAccount.cs
+++ SavingsAccount.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace Ovning_OPP_Github
 {
+    // Arv klass från BankAccount
     public class SavingsAccount : BankAccount
     {
+        // Fältet för att lagra kontobalansen
         private int balance;
 
+        // Konstruktor för att initiera sparkontot med ett startbelopp
         public SavingsAccount(int Balance) : base(Balance)
         {
+            // Sätt kontobalansen till det angivna startbeloppet
             balance = Balance;
         }
     }

1481c1214b430b02c9875005c01e591ad398dc85
BorvD <xenomorph_85@hotmail.com>
2025-09-19 10:23:40 +0200

    Skapade Bank Account

--- BankAccount.cs
+++ BankAccount.cs
@@ -8,6 +8,37 @@ namespace Ovning_OPP_Github
 {
     internal class BankAccount
     {
+        // Fältet för att lagra kontobalansen
+        private int balance;
 
+        // Konstruktor för att initiera kontot med ett startbelopp
+        public BankAccount(int Balance)
+        {
+            balance = Balance;
+        }
+
+        // Metod för att sätta in pengar på kontot
+        public void Deposit(int amount)
+        {
+            balance += amount;
+        }
+
+        // Metod för att ta ut pengar från kontot
+        public void Withdraw(int amount)
+        {
+            balance -= amount;
+        }
+
+        // Metod för att få fram kontobalansen
+        public int GetBalance()
+        {
+            return balance;
+        }
+
+        // Metod för att visa kontobalansen
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Deposit:{Deposit}, Withdraw:{Withdraw}Current balance: {balance}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // För att köra BankAccount-klassen
+            // Nytt konto med en balans på 100
+            BankAccount newAccount = new BankAccount(100);
+            // Sätter in 50 på kontot
+            newAccount.Deposit(50);
+            // Tar ut 25 från kontot
+            newAccount.Withdraw(25);
+            // Visar den aktuella balansen
+            newAccount.ShowBalance();
         }
     }
 }
